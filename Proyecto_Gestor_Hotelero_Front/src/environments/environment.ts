ed93409255c30999e62e2d41127d8ba22deb3938
jruiz-2019069 <jruiz-2019069@kinal.edu.gt>
2022-07-18 08:35:41 -0600

    Se modificaron los archivos enviroments.

--- Proyecto_Gestor_Hotelero_Front/src/environments/environment.ts
+++ Proyecto_Gestor_Hotelero_Front/src/environments/environment.ts
@@ -4,5 +4,13 @@
 
 export const environment = {
   production: false,
-  baseUri: 'https://gestor-hotelero.herokuapp.com/'
+  baseUri: 'https://gestor-hotelero.herokuapp.com/',
+  firebaseConfig: {
+    apiKey: "AIzaSyC2KWXa6Ho1i-RG6x3bYpZWO_ySgzd8uAA",
+    authDomain: "gestor-hotelero-c053e.firebaseapp.com",
+    projectId: "gestor-hotelero-c053e",
+    storageBucket: "gestor-hotelero-c053e.appspot.com",
+    messagingSenderId: "743847355167",
+    appId: "1:743847355167:web:64697210a048f476e3862a"
+  }
 };
